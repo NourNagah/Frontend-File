9f05ca1d00981da97df59a10a529254363a45982
Nour Nagah <nournagah89@gmail.comgit config --global user.name Nour>
2022-12-26 21:32:17 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Hellooozz")
+
+const submitButton=document.getElementById("submitButton");
+const emailInput=document.getElementById("emailInput");
+const passwordInput=document.getElementById("passwordInput");
+
+
+
+submitButton.addEventListener('click',emailInput);
+submitButton.addEventListener('click',passwordInput);
+//addeventlistener fel onclick bt3ha, 
