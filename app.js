8c67d77de317095f9e05e88d79366e2643658836
Nour Nagah <nournagah89@gmail.comgit config --global user.name Nour>
2022-12-26 21:53:57 +0200

    Updated app.js and index.html

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ const passwordInput=document.getElementById("passwordInput");
 submitButton.addEventListener('click',emailInput);
 submitButton.addEventListener('click',passwordInput);
 //addeventlistener fel onclick bt3ha,
-
+btn = document.getElementById('btn');
 
 btn.addEventListener('click',async()=>{
     console.log("hello")
-    const response = await fetch("http://localhost:3000/registration", {
+    const response = await fetch("http://localhost:3000/login", {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: `{
-       "email": "mmm@gmail.com",
-       "password":"12345",
+       "email": "nournagah89@gmail.com",
+       "password":"1234",
       }`,
     });
     
